3fce913dbe4c6554008a4bddec0738c9f6307bfd
nithin9066 <nithin9066@gmail.com>
2023-01-29 12:53:49 +0530

    final build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [output, setOutput] = useState(0);
+  const numberOfAlpha = [
+    ["a","j","s"],
+    ["b","k","t"],
+    ["c","l","u"],
+    ["d","m","v"],
+    ["e","n","w"],
+    ["f","o","x"],
+    ["g","p","y"],
+    ["h","q","z"],
+    ["i","r"],
+  ];
+
+  function stringToNumerology(e)
+  {
+    var out = 0;
+    var numbers = '';
+    const input = (e.target.value).toLowerCase();
+    for(var i = 0; i<input.length; i++)
+    {
+    for(var j = 0; j<numberOfAlpha.length; j++){
+        if(numberOfAlpha[j].includes(input[i]))
+        {
+          out += j + 1;
+        }
+        else if(parseInt(input[i]))
+        {
+          numbers += input[i];
+          break;
+        }
+      }
+    }
+    console.log(out);
+    out = out.toString()+numbers;
+    var outp = 0;
+    console.log(out);
+    outp = makeSingleDigit(out);
+    setOutput(outp);
+  }
+
+  function makeSingleDigit(out)
+  {
+    var result = 0;
+    for(var i = 0; i<out.length; i++)
+    {
+      result += parseInt(out[i]);
+    }
+    if(result < 10)
+    {
+      return result;
+    }
+    else
+    {
+      
+      return makeSingleDigit(result.toString())
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen grid place-items-center" style={{ backgroundImage: `url('background.svg')` }}>
+      <div className="w-96 bg-sky-400/60 p-10 rounded-xl space-y-10 shadow-xl">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="" className="font-medium text-white">Enter You String:</label>
+          <input className="px-3 py-2 rounded-md border outline-none" placeholder="Enter String" type="text" onChange={stringToNumerology} />
+        </div>
+        <div className="flex flex-col items-center gap-3 justify-center">
+          {
+            output ?
+            <>
+            <h5 className="font-bold text-white uppercase">Numerology number</h5>
+            <h4 className="font-bold text-sky-500 grid place-items-center h-14 rounded-full w-14 text-xl bg-white">{output}</h4>
+            </>
+            : ''
+          }
+          
+        </div>
+      </div>
     </div>
   );
 }
